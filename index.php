117364e50e6b81e8d34c2f1cc249af9455cf7816
KayTo <74450324+KayToCode@users.noreply.github.com>
2024-09-20 11:04:38 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Github Desktop</h1>
+    <p>inviter l'étudiant 2</p>
+</body>
+</html>
